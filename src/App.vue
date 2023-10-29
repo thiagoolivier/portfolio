aa546e01802f7f3587d76b4115eb3d339697ca03
thiagoolivier <thiagoolivier53@gmail.com>
2023-10-29 16:45:44 -0400

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,23 @@
-<script setup>
-import LeftDiv from "./layouts/LeftDiv.vue";
-import RightDiv from "./layouts/RightDiv.vue";
-import TranslateButton from "./components/TranslateButton.vue";
-</script>
-
 <template>
-  <div class="md:max-h-[100vh] py-12 lg:px-2">
-    <div class="grid lg:grid-cols-2 lg:max-w-6xl lg:mx-auto max-lg:mx-2">
+  <div class="md:max-h-[100vh] py-6 lg:px-2">
+    <div class="grid lg:grid-cols-1 lg:max-w-6xl lg:mx-auto max-lg:mx-2">
       <TranslateButton />
     </div>
     <div class="grid gap-4 lg:grid-cols-2 lg:max-w-6xl lg:mx-auto mt-4">
       <LeftDiv class="max-lg:mx-2" />
       <RightDiv class="rounded-lg lg:overflow-y-auto lg:h-[80vh] scrollbar-hide" />
     </div>
+    <footer class="grid lg:grid-cols-1 lg:max-w-6xl lg:mx-auto max-lg:mx-2">
+      <p class="py-4 text-center">
+        <small class="text-xs normal-text align-bottom">Made with Vue.js and Love </small>
+        <img class="inline w-5" src="/images/heart-tank.gif" alt="Megaman-x heart tank GIF.">
+      </p>
+    </footer>
   </div>
 </template>
 
-<style scoped>
-/* For Webkit-based browsers (Chrome, Safari and Opera) */
-.scrollbar-hide::-webkit-scrollbar {
-  display: none;
-}
-
-/* For IE, Edge and Firefox */
-.scrollbar-hide {
-  -ms-overflow-style: none;
-  /* IE and Edge */
-  scrollbar-width: none;
-  /* Firefox */
-}
-</style>
+<script setup>
+import LeftDiv from "./layouts/LeftDiv.vue";
+import RightDiv from "./layouts/RightDiv.vue";
+import TranslateButton from "./components/TranslateButton.vue";
+</script>
