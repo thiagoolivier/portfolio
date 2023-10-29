a6475fd64891b27d6ece2a99e9024c4e2bcfb864
thiagoolivier <thiagoolivier53@gmail.com>
2023-10-29 16:46:21 -0400

    Creates text copy component and logic

--- src/components/ContactOption.vue
+++ src/components/ContactOption.vue
@@ -1,51 +1,61 @@
-<script setup>
+<template>
+  <div class="group flex grow items-center space-x-6 justify-between relative" :title="title">
+    <button type="button">
+      <a :href="url" target="_blank">
+        <slot name="contact-icon" class="svg-icon"></slot>
+      </a>
+    </button>
 
+    <span ref="linkText"
+      class="transition ease-in delay-[50ms] text-sm font-medium text-[#8c8a8a] group-hover:text-white">
+      <slot name="link-text"></slot>
+    </span>
+
+    <button ref="copyBtn" @click="copyText" title="Copy content" id="copy-btn" class="inline-block">
+      <svg class="svg-icon" id="copy-icon" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M15.2728 4H6.54549C5.74549 4 5.09094 4.65455 5.09094 5.45455V15.6364H6.54549V5.45455H15.2728V4ZM17.4546 6.90909H9.45458C8.65458 6.90909 8.00003 7.56364 8.00003 8.36364V18.5455C8.00003 19.3455 8.65458 20 9.45458 20H17.4546C18.2546 20 18.9091 19.3455 18.9091 18.5455V8.36364C18.9091 7.56364 18.2546 6.90909 17.4546 6.90909ZM17.4546 18.5455H9.45458V8.36364H17.4546V18.5455Z" />
+      </svg>
+    </button>
+    <Transition name="copy">
+      <CopyAlert v-if="isClicked"/>
+    </Transition>
+  </div>
+</template>
+
+<script setup>
 import { ref } from 'vue';
-import { onClickOutside } from '@vueuse/core';
+import CopyAlert from './CopyAlert.vue';
 
 const props = defineProps({
   url: String,
   title: String,
 })
 
+const linkText = ref(null);
+const copyBtn = ref(null);
+const isClicked = ref(false);
+
+const copyText = () => {
+  navigator.clipboard.writeText(linkText.value.innerText);
+  isClicked.value = true;
+  setTimeout(() => {
+    isClicked.value = false;
+  }, 2000)
+}
 </script>
 
-<template>
-    <Transition name="contact-item">
-      <div class="flex grow items-center space-x-6 justify-between" :title="title">
-        <button type="button">
-          <a :href="url" target="_blank">
-            <slot name="contact-icon"></slot>
-          </a>
-        </button>
-
-        <span class="text-sm font-medium text-[#8c8a8a]">
-          <slot name="link-text"></slot>
-        </span>
-
-        <span class="inline-block">
-          <svg id="copy-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M15.2728 4H6.54549C5.74549 4 5.09094 4.65455 5.09094 5.45455V15.6364H6.54549V5.45455H15.2728V4ZM17.4546 6.90909H9.45458C8.65458 6.90909 8.00003 7.56364 8.00003 8.36364V18.5455C8.00003 19.3455 8.65458 20 9.45458 20H17.4546C18.2546 20 18.9091 19.3455 18.9091 18.5455V8.36364C18.9091 7.56364 18.2546 6.90909 17.4546 6.90909ZM17.4546 18.5455H9.45458V8.36364H17.4546V18.5455Z"
-              fill="#8C8A8A" />
-          </svg>
-        </span>
-      </div>
-    </Transition>
-</template>
-
-<style>
-.lang-card {
-  @apply absolute -top-10 left-10 inline-block rounded p-2 bg-[#3A3A3A]
+<style scoped>
+.copy-enter-active {
+  transition: opacity 200ms ease-in;
 }
 
-.lang-enter-active,
-.lang-leave-active {
-  transition: opacity 200ms ease-out;
+.copy-leave-active {
+  transition: opacity 600ms ease-in;
 }
 
-.lang-enter-from,
-.lang-leave-to {
+.copy-enter-from,
+.copy-leave-to {
   opacity: 0;
 }
 </style>
---
+++ src/components/CopyAlert.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="badge -right-7 -top-8 w-max py-1 px-1 text-xs font-bold shadow-md">
+    Text copied
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.badge {
+  display: inline-block;
+  background-color: #8C8A8A;
+  color: #1E1E1E;
+  border: 1px solid #8C8A8A;
+  border-radius: 10px;
+  position: absolute;
+}
+
+.badge::before {
+  content: "";
+  position: absolute;
+  top: 120%;
+  right: 45%;
+  margin-top: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: transparent transparent transparent #8C8A8A;
+  transform: rotate(90deg);
+}
+</style>
